f14a8a2d1aad936241d36e4e1d1c5678801ff11f
xmzhang <25337942@qq.com>
2022-02-20 17:12:25 +0800

    【add】完成模型类的编写

--- main.go
+++ main.go
@@ -1,8 +1,55 @@
 package main
 
-import "github.com/Shimon-Cheung/blog-service/internal/routers"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Shimon-Cheung/blog-service/global"
+	"github.com/Shimon-Cheung/blog-service/internal/routers"
+	"github.com/Shimon-Cheung/blog-service/pkg/setting"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+}
 
 func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
-	router.Run()
+	s := &http.Server{
+		Addr:           ":" + global.ServerSetting.HttpPort,
+		Handler:        router,
+		ReadTimeout:    global.ServerSetting.ReadTimeout,
+		WriteTimeout:   global.ServerSetting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
 }
